99f5232e9a2293636cf81baa8270f7e1abe98e68
Qassim.Dev <Qassim.Dev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-22 06:49:05 +0000

    readme shortcode-to-widget
    git-svn-id: https://plugins.svn.wordpress.org/shortcode-to-widget/trunk@1141792 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,69 @@
+﻿=== Shortcode To Widget ===
+Contributors: Qassim.Dev
+Donate link: http://j.mp/WPTime_donations
+Tags: shortcode widget, shortcode to widget, shortcodes, facebook, perso slider, slider, contact, contact form, contact form 7, form, widgets, image, images, shortcode, Post, plugin, posts, page, widget, admin, sidebar, google, twitter, comments
+Requires at least: 3.0
+Tested up to: 4.1.2
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Display any shortcode to your sidebar easily.
+
+== Description ==
+
+### Shortcode To Widget  
+
+Display any shortcode to your sidebar easily,
+for example display [Perso Slider](https://wordpress.org/plugins/perso-slider/) or [Facebook Post Embed](https://wordpress.org/plugins/facebook-post-embed/) or [Contact Form 7](https://wordpress.org/plugins/contact-form-7/) or any shortcode to your sidebar.
+
+### The Features
+
+* Display any shortcodes to sidebar.
+* Easy to use.
+
+### Live Demo
+
+* [http://wp-time.com/shortcode-to-widget/](http://wp-time.com/shortcode-to-widget/)
+
+### Rate The Plugin
+
+* [Please rate Shortcode To Widget plugin](https://wordpress.org/support/view/plugin-reviews/shortcode-to-widget#postform)
+
+### See Also
+
+* [Collection of 87 themes for $69 only.](http://j.mp/ET_WPTime)
+* [Premium WordPress themes on Creative Market.](http://j.mp/CM_WPTime)
+* [Premium WordPress themes on Themeforest.](http://j.mp/TF_WPTime)
+* [Premium WordPress plugins on Codecanyon.](http://j.mp/CC_WPTime)
+* [Unlimited web hosting for $3.95 only.](http://j.mp/BH_WPTime)
+
+### About
+
+* [The plugin designed and developed by Qassim Hassan.](http://qass.im)
+* [Qassim Hassan on Twitter.](https://twitter.com/Qassim_Dev)
+
+### More Plugins
+
+* [My Plugins](https://profiles.wordpress.org/qassimdev/#content-plugins)
+
+== Installation ==
+
+### Installation
+
+1. Upload 'shortcode-to-widget' folder to the '/wp-content/plugins/' directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Go to Appearance > Widgets > Shortcode To Widget.
+4. Drag and drop "Shortcode To Widget" widget and open it and enter your shortcode.
+
+== Frequently Asked Questions ==
+
+### FAQ
+
+* [For more questions or help, contact me.](http://wp-time.com/contact/)
+* [Or ask me on twitter.](https://twitter.com/Qassim_Dev)
+
+== Changelog ==
+
+= 1.0 =
+First version.
---
+++ shortcode-to-widget.php
@@ -0,0 +1,94 @@
+<?php
+/*
+Plugin Name: Shortcode To Widget
+Plugin URI: http://wp-time.com/shortcode-to-widget/
+Description: Display any shortcode to your sidebar easily.
+Version: 1.0
+Author: Qassim Hassan
+Author URI: http://qass.im
+License: GPLv2 or later
+*/
+
+/*  Copyright 2015 Qassim Hassan (email: qassim.pay@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+// Shortcode to widget
+class WPTimeShortcodeToWidget extends WP_Widget {
+	function WPTimeShortcodeToWidget() {
+		parent::__construct( false, 'Shortcode To Widget', array('description' => 'Display any shortcode to sidebar.') );
+	}
+
+	function widget( $args, $instance ) {
+		extract( $args );
+		$title = $instance['title'];
+		$shortcode = $instance['shortcode'];
+
+		?>
+            
+			<?php echo $args['before_widget'] . $args['before_title'] . $title . $args['after_title']; ?>
+            
+			<?php 
+				if( empty($shortcode) ){
+					echo "<ul><li>Please enter your shortcode.</li></ul>";
+				}else{
+					echo do_shortcode($shortcode);
+				}
+			 ?>
+            
+            <?php echo  $args['after_widget']; ?>
+            
+        <?php
+	}//widget
+	
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['shortcode'] = $new_instance['shortcode'];
+		return $instance;
+	}//update
+	
+	function form( $instance ) {
+		$instance = wp_parse_args(
+			(array) $instance
+		);
+		
+		$defaults = array(
+			'title' 	=> 'Shortcode To Widget',
+			'shortcode' => ''
+		);
+		
+		$instance = wp_parse_args( (array) $instance, $defaults );
+		$title = $instance['title'];
+		$shortcode = $instance['shortcode'];
+		?>
+			<p>
+				<label for="<?php echo $this->get_field_id('title'); ?>">Title:</label> 
+				<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+			</p>
+            
+			<p>
+				<label for="<?php echo $this->get_field_id('shortcode'); ?>">Shortcode:</label> 
+				<textarea class="widefat" rows="16" cols="20" id="<?php echo $this->get_field_id('shortcode'); ?>" name="<?php echo $this->get_field_name('shortcode'); ?>"><?php echo $shortcode; ?></textarea>
+			</p>
+        <?php
+		
+	}//form
+	
+}
+add_action('widgets_init', create_function('', 'return register_widget("WPTimeShortcodeToWidget");') );
+
+?>
